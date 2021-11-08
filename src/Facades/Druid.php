23c841111c898c825cac64f5e4089d556af62188
Ali Baraghithi <ali@autoleadstar.com>
2021-11-08 18:00:57 +0200

    add method documentation to facade

--- src/Facades/Druid.php
+++ src/Facades/Druid.php
@@ -12,6 +12,20 @@ use Illuminate\Support\Facades\Facade;
 /**
  * Class Druid
  *
+ * @method static \Level23\Druid\Queries\QueryBuilder query(string $dataSource, string $granularity = \Level23\Druid\Types\Granularity::ALL)
+ * @method static void cancelQuery(string $identifier)
+ * @method static array executeQuery(\Level23\Druid\Queries\QueryInterface $druidQuery)
+ * @method static string executeTask(\Level23\Druid\Tasks\TaskInterface $task)
+ * @method static array executeRawRequest(string $method, string $url, array $data = [])
+ * @method static \Level23\Druid\DruidClient setLogger(LoggerInterface $logger)
+ * @method static \Level23\Druid\DruidClient setGuzzleClient(GuzzleClient $client)
+ * @method static mixed function config(string $key, $default = null)
+ * @method static \Level23\Druid\Metadata\MetadataBuilder metadata()
+ * @method static \Level23\Druid\Responses\TaskResponse taskStatus(string $taskId)
+ * @method static \Level23\Druid\Tasks\CompactTaskBuilder compact(string $dataSource)
+ * @method static \Level23\Druid\Tasks\KillTaskBuilder kill(string $dataSource)
+ * @method static \Level23\Druid\Tasks\IndexTaskBuilder reindex(string $dataSource)
+ *
  * @package Level23\Druid\Facades
  * @codeCoverageIgnore
  */
